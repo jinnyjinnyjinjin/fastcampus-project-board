a7b21627b1f4f02e1b0270c40f8f087b00b23620
jinnyjinnyjinjin <kkam2ya@gmail.com>
2023-01-09 23:22:09 +0900

    #8-도메인 클래스 설계(게시글, 댓글)
    도메인 정보를 설계 해 보았다.
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB와 연동을 위한 추가적인 표현은 추후에 업데이트 될 수 있음

---
+++ src/main/java/com/jinnyjinnyjinjin/projectboard/domain/ArticleComment.java
@@ -0,0 +1,13 @@
+package com.jinnyjinnyjinjin.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+    private Article article;
+    private String content;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedByl;
+}
